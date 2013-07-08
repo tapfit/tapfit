4846687c907a0b9f40bc67f0871e908fd92e5e30
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2013-07-08 10:29:57 -0500

    updated unicorn file

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,6 +10,11 @@ before_fork do |server, worker|
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
+
+  if defined?(Resque)
+    Resque.redis.quit
+    Rails.logger.info("Disconnected from Redis")
+  end
 end 
 
 after_fork do |server, worker|
@@ -19,4 +24,10 @@ after_fork do |server, worker|
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
+
+  if defined?(Resque)
+    uri = URI.parse(ENV["REDISTOGO_URL"])
+    Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+    Rails.logger.info("Connected to Redis")
+  end
 end
