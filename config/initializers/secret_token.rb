643ce8d786abd67d66bfeac14b9b4788426815a3
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2013-07-08 09:40:04 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tapfit::Application.config.secret_key_base = 'cef92ea2a5a1a9f312056c05a4e346649c07bf6a4b0ea59038e02308967358d2de777c1669ff1e4d2e8cd86f61693fef085aae94a757f7399c4cbdde53568984'
