a8d1238633777045b3d49659ac9084f9b63b4c36
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2013-07-15 07:55:56 -0500

    Added places controller

---
+++ app/controllers/api/v1/places_controller.rb
@@ -0,0 +1,33 @@
+module Api
+  module V1
+    class PlacesController < ApplicationController
+      
+      respond_to :json
+
+      # GET places/
+      # Params: lat, lon. Optional Params: auth_token, q, radius
+      # Returns array of place around you (default: cincinnati)
+      def index
+        @places = Places.get_nearby_places(params[:lat], params[:lon])
+        render @places.as_json
+      end
+      
+      def show
+
+      end
+
+      def create
+
+      end
+
+      def update
+
+      end
+
+      def destroy
+
+      end
+
+    end
+  end
+end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,14 @@
 class Place < ActiveRecord::Base
   acts_as_taggable_on :categories
+  belongs_to :address
+
+  def get_nearby_places(lat, lon)
+    return Place.nearby(lat, lon, 0.05)  
+  end
+
+  scope :nearby, lambda { |lat, lon, radius|
+      where("address.lat BETWEEN ? AND ?", lat - radius, lat + radius).
+      where("address.lon BETWEEN ? AND ?", lon - radius, lon + radisu)
+  }
+
 end
