87dc3bf3be0fb479b22f96616a75eb4ec2beca52
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2013-07-15 09:27:48 -0500

    Added json method to place model

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,10 +6,27 @@ class Place < ActiveRecord::Base
     return Place.nearby(lat.to_f, lon.to_f, 0.05)  
   end
 
+  def next_class
+    return Workout.where(:place_id => self.id).order("start_time DESC").first
+  end
+
   scope :nearby, lambda { |lat, lon, radius|
       joins(:address).
       where("lat BETWEEN ? AND ?", lat - radius, lat + radius).
       where("lon BETWEEN ? AND ?", lon - radius, lon + radius)
   }
 
-end
+  def as_json(options={})
+    if !options[:list].nil?
+      except_array ||= [ :url, :category, :phone_number, :source, :source_key, :tapfit_description, :source_description, :is_public, :can_dropin, :dropin_price, :created_at, :updated_at, :address_id ]
+      options[:include] ||= [ :address ]
+      options[:methods] ||= [ :next_class ]
+    elsif !options[:detail].nil?      
+      except_array ||= [ :created_at, :updated_at, :latitude, :longitude, :physical_address_id, :billing_address_id ]
+      options[:include] ||= [ :physical_address, :billing_address, :business_hours, :amenities ]
+      options[:methods] ||= [ :avg_rating ]
+    end
+    options[:except] ||= except_array
+    super(options)
+
+  endend
