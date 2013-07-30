39b642f31a6fd6ab22d5126d54ed289b6cd48568
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2013-07-30 13:54:45 -0500

    Change application.rb

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,7 @@ module Tapfit
     
     config.assets.initialize_on_precompile = false
 
-    config.assets.precompile += ['active_admin.css', 'active_admin.js']
+    config.assets.precompile += %w['active_admin.css', 'active_admin.js', 'print.css']
+    config.assets.precompile += %w['active_admin/print.css']
   end
 end
