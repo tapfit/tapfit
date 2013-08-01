fcc88a8bc029de2740e7b4c3a5622369e1a2acb1
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2013-08-01 10:05:29 -0500

    Prevents duplicate workouts being saved to database

--- lib/crawler_validation/process_class.rb
+++ lib/crawler_validation/process_class.rb
@@ -40,6 +40,11 @@ class ProcessClass < ProcessBase
       ends = Time.zone.now.beginning_of_day.advance(:hours => @end_time.hour, :minutes => @end_time.strftime("%M").to_i)
 
       Time.zone = "UTC"
+      workout = Workout.where(:workout_key = workout_key).where(:start_time => starts).where(:end_time => ends).first
+      if !workout.nil?
+        puts "Workout already exists"
+        return
+      end
       workout = Workout.new(:name => @name, :place_id => @place_id, :source_description => @source_description, :start_time => starts.utc, :end_time => ends.utc, :price => @price, :instructor_id => instructor.id, :source => @source, :workout_key => workout_key)
 
       if !workout.valid?
