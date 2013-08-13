3fac1d8697ca59bac685347e14cb0ed96791d767
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2013-08-13 09:04:46 -0500

    Changed root url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Tapfit::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'evening-shore-3868.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'staging.tapfit.co' }
 
   config.paperclip_defaults = {
     :storage => :s3,
