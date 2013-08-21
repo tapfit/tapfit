ad9cbd7fb9aabb8cb036ef10797435f03557ed41
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2013-08-21 12:36:53 -0500

    Tweaked follow algo to never follow the same person twice

--- lib/twitter/follow.rb
+++ lib/twitter/follow.rb
@@ -41,6 +41,9 @@ module Follow
 
     search = "500Startups"
 
+
+    have_followed = REDIS.lrange("follow", 0, REDIS.llen("follow")).map { |int| int.to_i }
+
     response = access_token.request(:get, "https://api.twitter.com/1.1/followers/ids.json?user_id=168857946")
 
     json = JSON.parse(response.body.to_str)
@@ -51,9 +54,10 @@ module Follow
       if num < 10
         response = access_token.request(:get, "https://api.twitter.com/1.1/users/show.json?user_id=#{id}")
         user = JSON.parse(response.body.to_str)
-        if user["following"] == false && user["follow_request_sent"] == false
+        if user["following"] == false && user["follow_request_sent"] == false && !have_followed.include?(id)
           access_token.request(:post, "https://api.twitter.com/1.1/friendships/create.json?user_id=#{id}")
           puts "following user: #{id}"
+          REDIS.lpush("follow", id)
           num = num + 1
         end
       else
