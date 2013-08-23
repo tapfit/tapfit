9f8b04e10eef49f92a59a01c9dc9a7ba5ed1e342
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2013-08-23 08:32:26 -0500

    Added ability to update all workout descriptions

--- app/admin/workouts.rb
+++ app/admin/workouts.rb
@@ -12,17 +12,30 @@ ActiveAdmin.register Workout do
     end 
 
     default_actions
+
+
   end
 
   filter :can_buy
   filter :place_name, :label => "Place Name", :as => :string
   filter :start_time
 
+  member_action :update_description, :method => :get do
+    workout = Workout.find(params[:id])
+    Workout.where(:workout_key => workout.workout_key).update_all(:source_description => workout.source_description)
+    redirect_to admin_workouts_path
+  end
+
+  action_item :only => :show do
+    link_to("Update Descriptions", update_description_admin_workout_path(workout)) if !workout.source_description.nil? 
+  end
+
   form do |f|
     f.inputs "Workout" do
       f.input :name
       f.input :source_description
       f.input :can_buy
+      f.input :start_time
       f.input :price
     end
 
