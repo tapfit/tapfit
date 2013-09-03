005b819fe983dbf57058001a7e3646dd7a74f97b
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2013-09-03 07:53:12 -0500

    Added more logging to price

--- lib/crawler_validation/process_base.rb
+++ lib/crawler_validation/process_base.rb
@@ -62,7 +62,7 @@ class ProcessBase
 
     if @valid_keys.include?("price")
       if !check_price?(@price)
-        MailerUtils.write_error("price", @price, source_name)
+        MailerUtils.write_error("price", @price, "#{source_name} from place: #{@place_id}")
         failed_processing = true
         puts "failed price"
       else
