f5313b88c976f0fdc4d0569bafbb98085045fa72
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2013-09-09 14:47:06 -0500

    Added receipt email test

--- spec/models/receipt_spec.rb
+++ spec/models/receipt_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Receipt do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before(:each) do
+    @place = FactoryGirl.create(:place)
+    @workout = FactoryGirl.create(:workout)
+    @user = FactoryGirl.build(:user)
+    @user.email = "scott@tapfit.co"
+    @user.save
+  end
+
+  it 'should send receipt email' do
+    receipt = Receipt.create(:place_id => @place.id, :workout_id => @workout.id, :workout_key => @workout.workout_key, :user_id => @user.id, :has_booked => false, :price => 10)
+
+    receipt.send_receipt_email 
+  end 
 end
