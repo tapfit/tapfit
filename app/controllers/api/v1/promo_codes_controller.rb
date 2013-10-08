2ad5d907e8002b654c90f10fddc8fde98952f96a
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2013-10-08 14:35:48 -0500

    Added random promo code

--- app/controllers/api/v1/promo_codes_controller.rb
+++ app/controllers/api/v1/promo_codes_controller.rb
@@ -24,8 +24,22 @@ module Api
             if (Credit.where(:promo_code_id => promo_code.id, :user_id => current_user.id).count > 0)
               render :json => { :error => "You have already used this promo code" }
             else
-              credit = Credit.create(:total => promo_code.amount, :user_id => current_user.id, :promo_code_id => promo_code.id)
-              render :json => { :user => current_user.as_json(:auth => true) }
+              if !promo_code.random_promo.nil?
+                random_number = promo_code.random_promo * 100
+                r = Random.new
+                random = r.rand(100)
+                puts "random_number: #{random_number}, random: #{random}"
+                if (random_number > random)
+                  credit = Credit.create(:total => promo_code.amount, :user_id => current_user.id, :promo_code_id => promo_code.id)
+                  render :json => { :user => current_user.as_json(:auth => true) }
+                else
+                  credit = Credit.create(:total => 0, :user_id => current_user.id, :promo_code_id => promo_code.id)
+                  render :json => { :error => "Sorry, you did not win. Thanks for playing! Follow us on twitter and facebook for more chances to win!" }
+                end
+              else
+                credit = Credit.create(:total => promo_code.amount, :user_id => current_user.id, :promo_code_id => promo_code.id)
+                render :json => { :user => current_user.as_json(:auth => true) }
+              end
             end
           end
         end
