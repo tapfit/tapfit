5712a1edaa26756b8bf0df27bb86b1982f67dbbe
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2013-10-29 14:36:31 -0500

    Added zenplanner scraper

---
+++ lib/crawler-helpers/zenplanner.rb
@@ -0,0 +1,60 @@
+module Zenplanner
+
+  def self.get_classes(url, place_id, date, source)
+    
+    url = "#{url}?VIEW=MONTH&DATE=#{date.strftime("%Y-%m-%d")}&FRAME=false"
+
+    cmd = "phantomjs ./lib/phantomjs/get_page_with_js.js '#{url}' dayLabel"
+
+    output = `#{cmd}`
+
+    doc = Nokogiri::HTML(output)
+
+    dayLabel = date.strftime("%-d")
+
+    puts "day: #{dayLabel}"
+    
+    content = doc.xpath("//div[@class='dayLabel']")
+
+    content.each do |text|
+      
+      if text.text == dayLabel
+        # puts "Found the date: #{dayLabel}"
+        text.parent.children.each do |child| 
+          if !child['class'].nil? && child['class'].include?("clickable") && !(child['class'].include?("sessionFull") || child['class'].include?("RED"))
+            if child.text.include?("AM")
+              time = child.text.split("AM")[0]
+              name = child.text.split("AM")[1].split("(")[0]
+              time = DateTime.parse("#{time.strip} AM")
+              Zenplanner.save_to_database(name, time, place_id)
+            elsif child.text.include?("PM")
+              time = child.text.split("PM")[0]
+              name = child.text.split("PM")[1].split("(")[0]
+              time = DateTime.parse("#{time.strip} PM")
+              Zenplanner.save_to_database(name, time, place_id)              
+            end
+          end
+        end
+      end
+
+    end 
+
+  end
+
+  def self.save_to_database(name, start_time, place_id)
+    opts = {}
+    opts[:start_time] = start_time
+    opts[:end_time] = start_time.advance(:hours => 1)
+
+    opts[:name] = name
+    opts[:instructor] = "PunchHouse Trainer"
+    opts[:source] = "zenplanner"
+    
+    opts[:place_id] = place_id
+    opts[:tags] = [ Category::MartialArts, Category::Cardio, Category::Sports, Category::Strength ]
+
+    process_class = ProcessClass.new(opts)
+    process_class.save_to_database("zenplanner")
+  end
+
+end
---
+++ spec/lib/crawler-helpers/zenplanner_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe Zenplanner do
+
+  it 'should have a class from zenplanner' do
+    place = FactoryGirl.create(:place)
+    address = FactoryGirl.create(:valid_address_with_coordinates)
+    pass_detail = FactoryGirl.build(:pass_detail)
+    place_contract = FactoryGirl.build(:place_contract)
+    place_contract.discount = 0.50
+    place_contract.place_id = place.id
+    place_contract.save
+    pass_detail.place_id = place.id
+    pass_detail.save
+    place.address = address
+    place.can_buy = true
+    place.dropin_price = 15
+    place.source = "goRecess"
+    place.save
+    place.pass_details.first.should_not be_nil
+
+    url = "https://thepunchhouse.sites.zenplanner.com/calendar.cfm"
+
+    Zenplanner.get_classes(url, place.id, DateTime.now, place.source)
+    
+  end
+
+end
