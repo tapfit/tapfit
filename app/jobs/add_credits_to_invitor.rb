17aaf05fda55e04943c503b5889cae8be257b0d1
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2013-11-14 12:33:36 -0600

    Added logging to resque

--- app/jobs/add_credits_to_invitor.rb
+++ app/jobs/add_credits_to_invitor.rb
@@ -6,8 +6,10 @@ class AddCreditsToInvitor < ResqueJob
   @queue = :credits
 
   def self.perform(user_id, pass_count)
+    puts "pass_count: #{pass_count}, user_id: #{user_id}"
     if (pass_count == 0)
       Credit.where(:user_id => user_id).each do |credit|
+        puts "credit attributes: #{credit.attributes}"
         if (!credit.promo_code.user_id.nil?)
           Credit.create(:promo_code_id => credit.promo_code.id, :total => credit.promo_code.amount, :user_id => credit.promo_code.user_id)
         end
