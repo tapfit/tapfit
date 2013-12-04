a312f9a7d14456362914103d3f08e2c420f25a70
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2013-12-04 16:53:41 -0600

    Added skeleton of email

--- app/jobs/send_package_confirm_email.rb
+++ app/jobs/send_package_confirm_email.rb
@@ -1,4 +1,4 @@
-
+require 'mandrill'
 require 'resque'
 require './lib/resque_job'
 
@@ -16,8 +16,99 @@ class SendPackageConfirmEmail < ResqueJob
     end
     
     PromoCode.create(:code => gift_code, :amount => package.fit_coins, :quantity => 1)
-    
-    # Send email with package id info
+      
+
+    if !gift_email.nil?
+
+      send_gift_receipt_email(gift_code, email, gift_email)
+
+      send_gift_email(gift_code, email, gift_email)
+    else
+      send_receipt_email(gift_code, email)
+    end
+
+  end
+
+  def send_receipt_email(gift_code, email)
+        
+    mandrill = Mandrill::API.new 
+    template_name = "tapfit-purchase-confirmation"
+    template_content = [{"content"=>"example content", "name" => "example name"}] 
+    message = {}
+    message["subject"] = "Purchase Confirmed: Thanks for your purchase!"
+    message["from_email"] = "support@tapfit.co"
+    message["from_name"] = "TapFit"
+    message["to"] = [ {"email" => email} ]
+    message["html"] = ""
+    message["text"] = ""
+    message["merge_vars"] = [{"vars"=>[{"content"=>self.workout.name, "name"=>"WORKOUT"},
+                                       {"content"=>self.place.name, "name"=>"PLACE"},
+                                       {"content"=>self.workout.start_time.strftime("%A"), "name"=>"WORKOUT_DAY"},
+                                       {"content"=>self.workout.start_time.strftime("%l:%M%P"), "name"=>"WORKOUT_TIME"},
+                                       {"content"=>self.created_at.strftime("%A, %B %-d, %Y"), "name"=>"ORDER_DATE"},
+                                       {"content"=>self.price, "name"=>"SUBTOTAL"},
+                                       {"content"=>self.price, "name"=>"TOTAL"}], "rcpt"=>self.user.email}]
+    message["merge"] = true
+    message["track_opens"] = true
+    message["track_clicks"] = true
+    async = true
+    ip_pool = "Main Pool"
+    mandrill.messages.send_template(template_name, template_content, message, async, ip_pool)
+
+  end
+
+  def send_gift_receipt_email(gift_code, email, gift_email)
+        
+    mandrill = Mandrill::API.new 
+    template_name = "tapfit-purchase-confirmation"
+    template_content = [{"content"=>"example content", "name" => "example name"}] 
+    message = {}
+    message["subject"] = "Purchase Confirmed: Thanks for your purchase!"
+    message["from_email"] = "support@tapfit.co"
+    message["from_name"] = "TapFit"
+    message["to"] = [ {"email" => email} ]
+    message["html"] = ""
+    message["text"] = ""
+    message["merge_vars"] = [{"vars"=>[{"content"=>self.workout.name, "name"=>"WORKOUT"},
+                                       {"content"=>self.place.name, "name"=>"PLACE"},
+                                       {"content"=>self.workout.start_time.strftime("%A"), "name"=>"WORKOUT_DAY"},
+                                       {"content"=>self.workout.start_time.strftime("%l:%M%P"), "name"=>"WORKOUT_TIME"},
+                                       {"content"=>self.created_at.strftime("%A, %B %-d, %Y"), "name"=>"ORDER_DATE"},
+                                       {"content"=>self.price, "name"=>"SUBTOTAL"},
+                                       {"content"=>self.price, "name"=>"TOTAL"}], "rcpt"=>self.user.email}]
+    message["merge"] = true
+    message["track_opens"] = true
+    message["track_clicks"] = true
+    async = true
+    ip_pool = "Main Pool"
+    mandrill.messages.send_template(template_name, template_content, message, async, ip_pool)
+
+  end
+
+  def send_gift_email(gift_code, email, gift_email)
+    mandrill = Mandrill::API.new 
+    template_name = "tapfit-purchase-confirmation"
+    template_content = [{"content"=>"example content", "name" => "example name"}] 
+    message = {}
+    message["subject"] = "Purchase Confirmed: Thanks for your purchase!"
+    message["from_email"] = "support@tapfit.co"
+    message["from_name"] = "TapFit"
+    message["to"] = [ {"email" => gift_email} ]
+    message["html"] = ""
+    message["text"] = ""
+    message["merge_vars"] = [{"vars"=>[{"content"=>self.workout.name, "name"=>"WORKOUT"},
+                                       {"content"=>self.place.name, "name"=>"PLACE"},
+                                       {"content"=>self.workout.start_time.strftime("%A"), "name"=>"WORKOUT_DAY"},
+                                       {"content"=>self.workout.start_time.strftime("%l:%M%P"), "name"=>"WORKOUT_TIME"},
+                                       {"content"=>self.created_at.strftime("%A, %B %-d, %Y"), "name"=>"ORDER_DATE"},
+                                       {"content"=>self.price, "name"=>"SUBTOTAL"},
+                                       {"content"=>self.price, "name"=>"TOTAL"}], "rcpt"=>self.user.email}]
+    message["merge"] = true
+    message["track_opens"] = true
+    message["track_clicks"] = true
+    async = true
+    ip_pool = "Main Pool"
+    mandrill.messages.send_template(template_name, template_content, message, async, ip_pool)
 
   end
 
