7c875f1fed3ee820c0dd8a2bcbcfa40f4c8e7def
Scott Lieberman <scottklieberman@gmail.com>
2013-12-19 14:07:00 -0800

    Added semicolon after places javascript window

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -166,7 +166,6 @@ function initialize() {
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById("map_canvas"), settings);
-    //var locations = places;
     
     $.each( places, function(key, value) {
 
