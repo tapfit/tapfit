add9dd103972745bea832912e6670fbeb239a9c6
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2014-01-09 17:45:43 -0600

    Finalized tracking

--- app/jobs/track_mobile_app_tracking.rb
+++ app/jobs/track_mobile_app_tracking.rb
@@ -4,15 +4,15 @@ class TrackMobileAppTracking
 
   def perform(opts)
 
-    # if ENV["ANALYTICS_ENV"] == "production" 
-      url = "http://hastrk1.com/serve?action=click&publisher_id=50166&site_id=47168&offer_id=275198&sub_site=#{opts[:utm_source]}&sub_campaign=#{opts[:utm_campaign]}&sub_publisher=#{opts[:utm_medium]}&destination_id=46694&unid=#{opts[:unid]}"
+    if ENV["ANALYTICS_ENV"] == "production" 
+      url = "http://hastrk1.com/serve?action=click&publisher_id=50166&site_id=47168&offer_id=275198&sub_site=#{opts[:utm_source]}&sub_campaign=#{opts[:utm_campaign]}&sub_publisher=#{opts[:utm_medium]}&destination_id=46654&unid=#{opts[:unid]}"
          
       begin 
         RestClient.get(url)
       rescue
         
       end
-    # end
+    end
 
   end
 end
