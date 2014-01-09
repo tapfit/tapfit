6923712db144f305d15e1cdb1ca3cb594df82c34
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2014-01-09 12:22:24 -0600

    Stopped tracking for testing analytics env

--- app/jobs/track_mobile_app_tracking.rb
+++ app/jobs/track_mobile_app_tracking.rb
@@ -4,8 +4,11 @@ class TrackMobileAppTracking
 
   def perform(opts)
 
-    url = "http://hastrk1.com/serve?action=click&publisher_id=50166&site_id=47168&offer_id=275198&sub_site=#{opts[:utm_source]}&sub_campaign=#{opts[:utm_campaign]}&sub_publisher=#{opts[:utm_medium]}&destination_id=46216&unid=#{opts[:unid]}"
+    if ENV["ANALYTICS_ENV"] == "production" 
+      url = "http://hastrk1.com/serve?action=click&publisher_id=50166&site_id=47168&offer_id=275198&sub_site=#{opts[:utm_source]}&sub_campaign=#{opts[:utm_campaign]}&sub_publisher=#{opts[:utm_medium]}&destination_id=46216&unid=#{opts[:unid]}"
 
-    RestClient.get(url)
+      RestClient.get(url)
+    end
+
   end
 end
