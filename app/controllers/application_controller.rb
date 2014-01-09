5e04d3b861246cc2c43324fea44c682031be335c
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2014-01-09 14:45:32 -0600

    Added .tapfit.co as cookie domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -52,7 +52,7 @@ class ApplicationController < ActionController::Base
   def get_source_info
 
     if cookies.signed[:distinct_id].nil?
-      cookies.permanent.signed[:distinct_id] = { :value => SecureRandom.base64, :domain => 'www.tapfit.co' }
+      cookies.permanent.signed[:distinct_id] = { :value => SecureRandom.base64, :domain => ".tapfit.co" }
     end
 
     @distinct_id = cookies.signed[:distinct_id]
