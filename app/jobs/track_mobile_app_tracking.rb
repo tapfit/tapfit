b551cdb99781cc6874e35e769a29bc77fe3692b0
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2014-01-09 13:44:39 -0600

    Got rid of redirecting loop

--- app/jobs/track_mobile_app_tracking.rb
+++ app/jobs/track_mobile_app_tracking.rb
@@ -5,9 +5,13 @@ class TrackMobileAppTracking
   def perform(opts)
 
     if ENV["ANALYTICS_ENV"] == "production" 
-      url = "http://hastrk1.com/serve?action=click&publisher_id=50166&site_id=47168&offer_id=275198&sub_site=#{opts[:utm_source]}&sub_campaign=#{opts[:utm_campaign]}&sub_publisher=#{opts[:utm_medium]}&destination_id=46216&unid=#{opts[:unid]}"
-
-      # RestClient.get(url)
+      url = "http://hastrk1.com/serve?action=click&publisher_id=50166&site_id=47168&offer_id=275198&sub_site=#{opts[:utm_source]}&sub_campaign=#{opts[:utm_campaign]}&sub_publisher=#{opts[:utm_medium]}&destination_id=46654&unid=#{opts[:unid]}"
+         
+      begin 
+        RestClient.get(url)
+      rescue
+        
+      end
     end
 
   end
