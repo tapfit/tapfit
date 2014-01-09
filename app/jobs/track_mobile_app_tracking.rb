0d7500ce1bd5046c133b14e21e8d9a96fab5352c
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2014-01-09 15:55:54 -0600

    Removed tracking

--- app/jobs/track_mobile_app_tracking.rb
+++ app/jobs/track_mobile_app_tracking.rb
@@ -8,7 +8,7 @@ class TrackMobileAppTracking
       url = "http://hastrk1.com/serve?action=click&publisher_id=50166&site_id=47168&offer_id=275198&sub_site=#{opts[:utm_source]}&sub_campaign=#{opts[:utm_campaign]}&sub_publisher=#{opts[:utm_medium]}&destination_id=46654&unid=#{opts[:unid]}"
          
       begin 
-        RestClient.get(url)
+        # RestClient.get(url)
       rescue
         
       end
