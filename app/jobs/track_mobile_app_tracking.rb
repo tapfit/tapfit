4646eb38e9c0f35231f6930ba63739280c70def5
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2014-01-09 12:42:36 -0600

    Removed tracking on mobile app tracking

--- app/jobs/track_mobile_app_tracking.rb
+++ app/jobs/track_mobile_app_tracking.rb
@@ -7,7 +7,7 @@ class TrackMobileAppTracking
     if ENV["ANALYTICS_ENV"] == "production" 
       url = "http://hastrk1.com/serve?action=click&publisher_id=50166&site_id=47168&offer_id=275198&sub_site=#{opts[:utm_source]}&sub_campaign=#{opts[:utm_campaign]}&sub_publisher=#{opts[:utm_medium]}&destination_id=46216&unid=#{opts[:unid]}"
 
-      RestClient.get(url)
+      # RestClient.get(url)
     end
 
   end
