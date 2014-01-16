00b04be3e1561250dc064d3ef8a2486967e97dca
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2014-01-16 09:04:41 -0600

    Random commit

--- db/schema.rb
+++ db/schema.rb
@@ -319,7 +319,6 @@ ActiveRecord::Schema.define(version: 20140115205013) do
 
   add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
   add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
-  add_index "taggings", ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type", using: :btree
 
   create_table "tags", force: true do |t|
     t.string "name"
