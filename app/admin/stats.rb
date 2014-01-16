b7608f7a60d75708c6a69cb44f09dddabf7773bf
Zachary Martinsek <zackmartinsek@guitar.motleeapp.com>
2014-01-15 18:09:56 -0600

    Remake of stats admin

--- app/admin/stats.rb
+++ app/admin/stats.rb
@@ -8,7 +8,18 @@ ActiveAdmin.register_page "Stats" do
 
   controller do
     def index
-      @stats = Stats.new
+      stop_date = DateTime.parse('2013-09-02')
+
+      date = DateTime.now.beginning_of_week + 1.weeks
+      @stats = []
+      while date > stop_date
+        @stats << Stats.new(date)
+        date = date - 1.weeks
+      end
+
+      @stats.each do |stat|
+        puts stat.wow_purchase_amount
+      end
     end
   end
 end
